c785aeaa87a49c668c6f1bc4a3bb5bffb6f92071
Chris Ellis <chris@intrbiz.com>
2020-05-17 17:31:54 +0100

    Correctly convert values for HW 2+

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -6,25 +6,32 @@ public class Example {
 
     public static void main(String[] args) throws Exception
     {
-        HS110Client plug = new HS110Client("172.30.4.139");
-        plug.sysInfo();
-        plug.on();
-        //
+        HS110Client plug1 = new HS110Client("172.30.4.139");
+        HS110Client plug2 = new HS110Client("172.30.4.140");
+        
+        System.out.println(plug1.sysInfo());
+        System.out.println(plug2.sysInfo());
+        
+        plug1.on();
+        plug2.on();
+        
         for (int i = 0; i < 10; i++)
         {
-            GetRealtime response = plug.consumption();
-            System.out.println("Power: " + response.getPower() + "W, Total Consumption: " + response.getConsumption() + "kWh Current: " + response.getCurrent() + "A Voltage: " + response.getVoltage());
+            GetRealtime response1 = plug1.consumption();
+            System.out.println("Power: " + response1.getPower() + "W, Total Consumption: " + response1.getConsumption() + "kWh, Current: " + response1.getCurrent() + "A, Voltage: " + response1.getVoltage() + "V");
+            GetRealtime response2 = plug2.consumption();
+            System.out.println("Power: " + response2.getPower() + "W, Total Consumption: " + response2.getConsumption() + "kWh, Current: " + response2.getCurrent() + "A, Voltage: " + response2.getVoltage() + "V");
             Thread.sleep(5000);
         }
         //
-        plug.ledOff();
+        plug1.ledOff();
         Thread.sleep(5000);
-        plug.ledOn();
+        plug1.ledOn();
         Thread.sleep(5000);
         //
-        plug.off();
+        plug1.off();
         Thread.sleep(5000);
-        plug.on();
+        plug1.on();
     }
 
 }
